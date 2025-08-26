043cf7072fa7227af3562408b44010caf8091cc2
Archa Chhaya <28archa28@gmail.com>
2025-08-26 15:02:32 +1000

    Commit 3 - Hoist the state and coroutine in viewmodel

--- app/src/main/java/com/example/mealsapp/ui/meals/MainActivity.kt
+++ app/src/main/java/com/example/mealsapp/ui/meals/MainActivity.kt
@@ -8,16 +8,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.mealsapp.model.response.MealsResponse
 import com.example.mealsapp.ui.theme.MealsAppTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,20 +27,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MealsCategoriesScreen() {
     val viewModel: MealsCategoriesViewModel = viewModel()
-    val rememberMeals = remember { mutableStateOf(emptyList<MealsResponse>()) }
+    val meals = viewModel.mealsState.value
 
     // the code inside LaunchEffect will execute only once and not everytime when recomposes
     // If the key changes, the code inside LaunchEffect will re-trigger
-    val coroutineScope = rememberCoroutineScope()
+    // but this is not a good practice so we are commenting code
+    // and instead hoist the state and coroutine in viewmodel
+
+    /*val coroutineScope = rememberCoroutineScope()
     LaunchedEffect(key1 = "GET_MEALS") {
         coroutineScope.launch(Dispatchers.IO) {
             val meals = viewModel.getMeals()
             rememberMeals.value = meals
         }
-    }
+    }*/
 
     LazyColumn {
-        items(rememberMeals.value) { meals ->
+        items(meals) { meals ->
             Text(text = meals.name)
         }
     }
--- app/src/main/java/com/example/mealsapp/ui/meals/MealsCategoriesViewModel.kt
+++ app/src/main/java/com/example/mealsapp/ui/meals/MealsCategoriesViewModel.kt
@@ -1,12 +1,30 @@
 package com.example.mealsapp.ui.meals
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mealsapp.model.MealsRepository
 import com.example.mealsapp.model.response.MealsResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()): ViewModel() {
 
-    suspend fun getMeals(): List<MealsResponse> {
+    init {
+        Log.d("TAG_COROUTINES", "we are about to launch a coroutine")
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("TAG_COROUTINES", "we have launched a coroutine")
+            val meals = getMeals()
+            Log.d("TAG_COROUTINES", "we have received the async data")
+            mealsState.value = meals
+        }
+        Log.d("TAG_COROUTINES", "other work")
+    }
+
+    val mealsState = mutableStateOf(emptyList<MealsResponse>())
+
+    private suspend fun getMeals(): List<MealsResponse> {
         return repository.getMeals().categories
     }
 }
