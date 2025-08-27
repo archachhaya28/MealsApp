aadf4b43e13c8db1ac04b7851ab8214a42811e5c
Archa Chhaya <28archa28@gmail.com>
2025-08-27 11:04:36 +1000

    Commit 5 - Added animation to the compose screen

--- app/src/main/java/com/example/mealsapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/example/mealsapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,6 +1,7 @@
 package com.example.mealsapp.ui.meals
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,16 +11,29 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil3.compose.rememberAsyncImagePainter
+import coil3.compose.AsyncImage
 import com.example.mealsapp.model.response.MealsResponse
 import com.example.mealsapp.ui.theme.MealsAppTheme
 
@@ -45,7 +59,7 @@ fun MealsCategoriesScreen() {
         }
     }*/
 
-    LazyColumn(contentPadding = PaddingValues(10.dp)) {
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
         items(meals) { meals ->
             MealsCategory(meals)
         }
@@ -54,27 +68,52 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealsCategory(meals: MealsResponse) {
+    var isExpanded by remember { mutableStateOf(false) }
     Card(
         shape = RoundedCornerShape(8.dp),
         modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
     ) {
-        Row {
-            Image(
-                painter = rememberAsyncImagePainter(meals.imageUrl),
+        // Modifier.animateContentSize() is to display the smooth animation
+        Row(modifier = Modifier.animateContentSize()) {
+            AsyncImage(
+                model = meals.imageUrl,
                 contentDescription = null,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(88.dp)
-                    .padding(4.dp)
+                    .size(80.dp)
+                    .align(Alignment.CenterVertically)
+
             )
             Column(modifier = Modifier
                 .align(Alignment.CenterVertically)
+                .fillMaxWidth(0.8f)
                 .padding(16.dp)
             ) {
                 Text(
                     text = meals.name,
-                    style = MaterialTheme.typography.displaySmall
+                    style = MaterialTheme.typography.titleLarge
                 )
+                CompositionLocalProvider(value = LocalTextStyle provides LocalTextStyle.current) {
+                    Text(
+                        text = meals.description,
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.titleMedium,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if (isExpanded) 10 else 4
+                    )
+                }
             }
+            Icon(imageVector = if
+                    (isExpanded) Icons.Filled.KeyboardArrowUp
+                else
+                    Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(if (isExpanded) Alignment.Bottom else Alignment.CenterVertically)
+                    .clickable { isExpanded = !isExpanded }
+                )
+
         }
     }
 }
